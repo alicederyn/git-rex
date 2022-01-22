7b2361e29f6faf424c0dada6d1c85c98eb01b8fc
Alice Purcell <Alice.Purcell.39@gmail.com>
2022-01-22 16:41:10 +0000

    Add code to grab a bash script from a git commit

--- git_rex/__init__.py
+++ git_rex/__init__.py
@@ -1,2 +1,86 @@
-def main():
-    print("Success!")
+"""Git Re-EXecute
+
+Alternative to git cherry-pick that finds and runs commands in the
+commit message, instead of reapplying the diff.
+
+Usage: git-rex COMMIT
+
+COMMIT   Commit to reexecute
+"""
+
+import re
+import sys
+
+from docopt import docopt
+from git_rex import git
+
+
+CODE_BLOCK = re.compile(r"\s*```(\w*)\s*$")
+
+
+class NoCodeFound(Exception):
+    pass
+
+
+class ScriptError(Exception):
+    def __init__(self, lineno: int, message: str):
+        self.lineno = lineno
+        self.message = message
+
+
+def extract_script(message: str):
+    """Extracts code from between triple-tick blocks
+
+    >>> commit_message = '''Sample commit
+    ...
+    ... This commit did some stuff
+    ...
+    ... ```bash
+    ... run_my_code thing
+    ... and_my_other thing
+    ... ```
+    ... '''
+    >>> extract_script(commit_message)
+    ('run_my_code thing', 'and_my_other thing')
+    """
+    in_code_block = False
+    lines = message.splitlines()
+    code_lines = []
+    for lineno, line in enumerate(lines):
+        if not in_code_block:
+            if m := CODE_BLOCK.match(line):
+                if m.group(1) != "bash":
+                    raise ScriptError(
+                        lineno, "Code sections must be specify bash syntax"
+                    )
+                in_code_block = True
+        else:
+            if m := CODE_BLOCK.match(line):
+                if m.group(1):
+                    raise ScriptError(lineno, "Unexpected start of new code section")
+                in_code_block = False
+            elif line.strip():
+                code_lines.append(line.strip())
+    if not code_lines:
+        raise NoCodeFound
+    return tuple(code_lines)
+
+
+def reexecute(commit_rev: str):
+    try:
+        commit = git.Commit(commit_rev)
+        script = extract_script(commit.message)
+        print(script)
+    except NoCodeFound:
+        print("fatal: No code section found in commit", file=sys.stderr)
+        sys.exit(64)
+    except ScriptError as e:
+        print(f"fatal: {e.lineno}: {e.message}", file=sys.stderr)
+        sys.exit(64)
+    except git.GitFailure as e:
+        print(f"fatal: {e.message}", file=sys.stderr)
+
+
+def main() -> None:
+    options = docopt(__doc__)
+    reexecute(commit_rev=options["COMMIT"])
---
+++ git_rex/git.py
@@ -0,0 +1,39 @@
+from subprocess import CalledProcessError, PIPE, Popen, check_output
+from functools import cached_property
+
+
+class GitFailure(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class Commit:
+    """Wrapper around Git commit commands.
+
+    >>> c = Commit("0e507f394bd3a3a4b96fe3208d14aab469ce1ab9")
+    >>> c.message
+    'Initial gitignore\\n'
+    """
+
+    def __init__(self, rev: str):
+        p = Popen(["git", "rev-parse", rev], stdout=PIPE, stderr=PIPE)
+        stdout, stderr = p.communicate()
+        if p.returncode != 0:
+            raise GitFailure(
+                stderr.decode("utf-8").splitlines()[0].removeprefix("fatal: ")
+            )
+        self.hash = stdout.decode("ascii").strip()
+
+    @cached_property
+    def message(self) -> bytes:
+        return check_output(
+            [
+                "git",
+                "show",
+                "--no-patch",
+                "--format=format:%B",
+                "--encoding=UTF-8",
+                self.hash,
+            ],
+            encoding="utf-8",
+        )
