3fe395576422199b617763c3d135051bc6dd24cf
Alice Purcell <Alice.Purcell.39@gmail.com>
2022-01-24 19:11:22 +0000

    Add system test

---
+++ test/system/conftest.py
@@ -0,0 +1,19 @@
+import sys
+from runpy import run_module
+
+import pytest
+
+import git_rex
+
+
+@pytest.fixture
+def rex():
+    def invoke_rex(*args: str):
+        argv_copy = list(sys.argv)
+        try:
+            sys.argv[:] = ["git-rex", *args]
+            git_rex.main()
+        finally:
+            sys.argv[:] = argv_copy
+
+    return invoke_rex
