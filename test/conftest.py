044a16326425d3ed182de5a1c137b2304138b3b9
Alice Purcell <Alice.Purcell.39@gmail.com>
2022-02-05 11:02:12 +0000

    Shrink four system tests to integration tests
    Four system tests were validating the behaviour of the bash script handling code, and can run as a more isolated test of that logic, reducing their complexity.

--- test/conftest.py
+++ test/conftest.py
@@ -20,14 +20,21 @@ def assert_git_version(minimum_version):
 
 
 @pytest.fixture
-def temp_git_repo(request, tmp_path):
+def temp_working_dir(request, tmp_path):
     assert_git_version("2.28")  # Needed for `git branch -m` to succeed
     os.chdir(tmp_path)
     try:
-        check_call(["git", "init", "--quiet"])
-        check_call(["git", "branch", "-m", "main"])
-        check_call(["git", "config", "user.email", "unit-test-runner@example.com"])
-        check_call(["git", "config", "user.name", "Unit Test Runner"])
+        os.mkdir(".git")  # Tests write temp files here
         yield tmp_path
     finally:
         os.chdir(request.config.invocation_dir)
+
+
+@pytest.fixture
+def temp_git_repo(temp_working_dir):
+    assert_git_version("2.28")  # Needed for `git branch -m` to succeed
+    check_call(["git", "init", "--quiet"])
+    check_call(["git", "branch", "-m", "main"])
+    check_call(["git", "config", "user.email", "unit-test-runner@example.com"])
+    check_call(["git", "config", "user.name", "Unit Test Runner"])
+    yield temp_working_dir
